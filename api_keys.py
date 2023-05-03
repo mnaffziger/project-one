6199d69c2f7a7ddbd52ceefcd43fe0f9f37dacd2
Mhills32 <raezay@gmail.com>
2023-05-03 11:43:46 -0700

    Upded Figs 1,2,3

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+api_key = "YwirNffQOK71QFvR0oQPj8ffdeCi4kX8aphqSxQm"
+
+
+weather_api_key= "a0a99476dfe39effca0d4f4b28079428"
+
+# Geoapify API Key
+geoapify_key = "e0efad8840d444df8ac0d81cf52eaebe"
